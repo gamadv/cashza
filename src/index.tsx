a7c656897d9aff25f1b4e60cf9c0739eb7d4ef04
Gamadv <lealdev.py@gmail.com>
2022-02-06 23:38:38 -0300

    feat: create endpoint on miragejs and set base database model to return saved data

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Raspagem de saco',
+          type: 'withdraw',
+          category: 'Estetica',
+          amount: 12000,
+          createdAt: new Date('2020-01-31'),
+        },
+        {
+          id: 2,
+          title: 'Boate',
+          type: 'deposit',
+          category: 'Freela',
+          amount: 12000,
+          createdAt: new Date('2020-01-31'),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'Deposit',
-          category: 'Food',
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     });
   },
 });
