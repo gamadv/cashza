3eaa6f6fb911a5a1de7b13737dc9e2dc19f3f7cd
gamadv <lealdev.py@gmail.com>
2023-05-02 10:14:56 -0300

    refactor: Change summary function to Hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,32 +1,10 @@
-import { useContext } from "react";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
-
-  console.log(transactions);
+  const { income, outcome, total } = useSummary();
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -35,7 +13,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{priceFormatter(summary.income)}</strong>
+        <strong>{priceFormatter(income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -44,7 +22,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{priceFormatter(summary.outcome)}</strong>
+        <strong>{priceFormatter(outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -53,7 +31,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{priceFormatter(summary.total)}</strong>
+        <strong>{priceFormatter(total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
